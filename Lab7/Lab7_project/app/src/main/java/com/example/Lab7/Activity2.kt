f3b3cd58584df32bce2d56cc72f80583e07eeb67
Сергей Самсонов <ser154nexus@gmail.com>
2021-04-13 20:18:34 +0300

    Slight changes

--- Lab7/Lab7_project/app/src/main/java/com/example/Lab7/Activity2.kt
+++ Lab7/Lab7_project/app/src/main/java/com/example/Lab7/Activity2.kt
@@ -2,10 +2,12 @@ package com.example.Lab7
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.Lab7.databinding.Activity2Binding
 
 class Activity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        val bi
+        val binding = Activity2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- Lab7/Lab7_project/app/src/main/java/com/example/Lab7/Activity3.kt
+++ Lab7/Lab7_project/app/src/main/java/com/example/Lab7/Activity3.kt
@@ -2,10 +2,12 @@ package com.example.Lab7
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.Lab7.databinding.Activity3Binding
 
 class Activity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        val binding =
+        val binding = Activity3Binding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
